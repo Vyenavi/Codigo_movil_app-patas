b6fdedd4ca9f85a512d4cc2b8281b6793b8ff803
JohanCV <nahoj1992@gmail.com>
2019-06-07 13:59:26 -0500

    version 1.0 master creacion de package

---
+++ app/src/main/java/com/app/apppatas/Inicio.java
@@ -0,0 +1,42 @@
+package com.app.apppatas;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.app.apppatas.home.HomeActivity;
+
+public class Inicio extends AppCompatActivity {
+
+    private EditText user, pass;
+    private Button btnlogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+
+        findElements();
+        events();
+    }
+
+    private void findElements() {
+        user = findViewById(R.id.edtxtUser);
+        pass = findViewById(R.id.edtxtPass);
+        btnlogin = findViewById(R.id.btnLogin);
+
+    }
+    private void events() {
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/app/apppatas/home/HomeActivity.java
@@ -0,0 +1,58 @@
+package com.app.apppatas.home;
+
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.app.apppatas.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        findElements();
+        events();
+    }
+
+    private void findElements() {
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+    }
+
+    private void events() {
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId()){
+                    case R.id.menu_inicio:
+                        Toast.makeText(HomeActivity.this, "Codigo de Home", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.menu_publicar:
+                        Toast.makeText(HomeActivity.this, "Codigo de Publicar", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.menu_tencontre:
+                        Toast.makeText(HomeActivity.this, "Codigo de Te encontre", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                return false;
+            }
+        });
+    }
+}
