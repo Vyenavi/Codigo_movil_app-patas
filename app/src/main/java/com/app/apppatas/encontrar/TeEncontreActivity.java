0f1a0f1525c1f8348a6e5a7d8c58cc8c97e1f602
Gohan-PC <nahoj1992@gmail.com>
2019-06-08 00:31:10 -0500

    version termino de estructurar proyecto

--- app/src/main/java/com/app/apppatas/encontrar/TeEncontreActivity.java
+++ app/src/main/java/com/app/apppatas/encontrar/TeEncontreActivity.java
@@ -1,15 +1,15 @@
-package com.app.apppatas.login;
+package com.app.apppatas.encontrar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.app.apppatas.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class TeEncontreActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_te_encontre);
     }
 }
