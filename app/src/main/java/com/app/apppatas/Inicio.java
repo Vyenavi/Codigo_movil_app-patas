66304718ae8cf4ec89fc9556f67b255158957144
johan <nahoj1992@gmail.com>
2019-07-13 11:12:29 -0500

    inicio agregando import
    inicio agregando import

--- app/src/main/java/com/app/apppatas/Inicio.java
+++ app/src/main/java/com/app/apppatas/Inicio.java
@@ -1,10 +1,10 @@
 package com.app.apppatas;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -25,6 +25,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
 
 public class Inicio extends AppCompatActivity {
 
