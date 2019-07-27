429dbf977af9820638d3d863ec0dbb150b4f62b4
alonsotitibenavente <48073344+alonsotitibenavente@users.noreply.github.com>
2019-07-26 20:31:04 -0500

    nuevos cambios

---
+++ app/src/main/java/com/app/apppatas/encontrar/vista_alertas.java
@@ -0,0 +1,30 @@
+package com.app.apppatas.encontrar;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.app.apppatas.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class vista_alertas extends Fragment {
+
+
+    public vista_alertas() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_vista_alertas, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/app/apppatas/encontrar/vista_localizador.java
@@ -0,0 +1,13 @@
+package com.app.apppatas.encontrar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class vista_localizador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista_localizador);
+    }
+}
